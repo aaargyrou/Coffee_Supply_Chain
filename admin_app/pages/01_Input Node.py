0eb7121570c431c4e3ccae3284b6af3fb5e95ec9
aaargyrou <aaargyrou@hotmail.com>
2022-07-28 17:34:34 +1000

    Continue Page 1 - To link to Smart Contract. To begin Transaction page

--- admin_app/pages/01_Input Node.py
+++ admin_app/pages/01_Input Node.py
@@ -6,6 +6,7 @@ from web3 import Web3
 from pathlib import Path
 from dotenv import load_dotenv
 import streamlit as st
+import pandas as pd
 
 # load environment variables
 load_dotenv()
@@ -35,21 +36,42 @@ def contract_load():
 
 #Load the contract
 contract = contract_load()
+df = pd.DataFrame()
 
 # Title
-st.title("Input Node")
+st.title("Input Business Details")
 
-# Text Input for contract Details
-st.subheader("Contract Details")
+# Sidebar Input for contract Details
+address = st.sidebar.selectbox("Business ETH address", options=addresses)
+name = st.sidebar.text_input('Name of Business')
+node_type = st.sidebar.selectbox('Business Type', options=['primaryProducer', 'Storage', 'Roaster', 'Retailer','Consumer'])
+latitude = st.sidebar.number_input('Business Latitude')
+longitude = st.sidebar.number_input('Business Longitude')
+coffeebag = st.sidebar.selectbox('Coffeebag', options=['True', 'False'])
+
+# Display the dataframe
+if st.sidebar.button("View Details"):
+    st.subheader("Contract Details")
+    df = df.append(
+        {'address': address, 'name': name, 'node_type': node_type, 'lat': latitude, 'lon': longitude, 'coffeebag': coffeebag}, ignore_index=True)
+    st.write(df)
+
+    # MAPBOX for Longitude and Latitude 
+    st.map(df)
 
 
-address = st.selectbox("Business ETH address", options=addresses)
-name = st.text_input('Name of Business')
-node_type = st.selectbox('Business Type', options=['primaryProducer', 'Storage', 'Roaster', 'Retailer','Consumer'])
-latitude = st.number_input('Business Latitude')
-longitude = st.number_input('Business Longitude')
+#TODO BUTTON TO LINK TO SMART CONTRACT AND STORE DATA IN THE BLOCKCHAIN
+#if st.sidebar.button("Add Business"):
+#    contract.functions.addNode(address, name, node_type, latitude, longitude).transact({'from': user_address})
+#    st.write("Business added")
 
-# Read all data inputed above
-with st.sidebar("Loading Contract Details"):
-    with st.spinner("Loading Contract Details"):
-        contract_details = w3.eth.get_contract_details(contract, address)
+
+
+# variables used in minting a batch
+# creator = st.selectbox("creator", options=addresses)
+# batch_uri = st.text_input("info or URI about batch")
+
+# button for transaction
+# if st.sidebar.button("mint a new batch NFT"):
+#    batch = coffee_contract.add_batch(creator, batch_uri)
+#    st.write(f'batch has been created!\n txn hash: {batch}')
---
+++ admin_app/pages/02_Transfer Coffee.py
@@ -0,0 +1,49 @@
+import os
+import json
+from web3 import Web3
+from pathlib import Path
+from dotenv import load_dotenv
+import streamlit as st
+
+# load environment variables
+load_dotenv()
+user_address = os.getenv("CONTRACT_USER_ADDRESS")
+w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
+# Get a list of addresses on the testnet
+addresses = w3.eth.accounts
+
+#Cache and load main contract 
+@st.cache(allow_output_mutation=True)
+
+#load contract function
+def contract_load():
+    #load abi
+    with open(Path('../contracts/compiled/coffeeChain.json')) as f: #compiled json file needs to be called contract_abi.json
+        contract_abi = json.load(f)
+
+    #Get contract address 
+    contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
+    
+    #Geting contract
+    contract = w3.eth.contract(
+        address=contract_address,
+        abi=contract_abi
+    )
+    return contract
+
+#Load the contract
+contract = contract_load()
+# streamlit section for testing NFT transfers
+st.sidebar.markdown("---")
+st.sidebar.markdown("send a batch to another address")
+
+# variables used in sending a batch
+owner = st.selectbox("address from", options=addresses)
+to = st.selectbox("address to", options=addresses)
+batch_num = st.number_input("select batch", min_value=0)
+
+# button for transaction
+if st.button("send a batch"):
+    sent_batch = contract.transfer_batch(owner, to, batch_num)
+    if sent_batch:
+        st.write(f'batch has been sent\n txn hash: {sent_batch}')
